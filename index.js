1cb0de26afe3edbf23551469a4d80a355f9b99d9
Satar Sarafa <sp3c7r40x00@gmail.com>
2024-10-28 14:41:54 +0100

    Hello git

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import express from 'express'
-import connectDB from './db/connectDB.js';
-import {createDoc, allDoc, singleDoc} from './models/Movies.js';
+
 
 const app = express()
-const port = process.env.PORT || 8000;
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/movies'
 
-connectDB(DATABASE_URL);
-// createDoc()
-// allDoc()
-singleDoc()
+app.get('/', (request, response) => {
+  response.send("Server working...")
+})
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+app.listen(3000, () => {
+  console.log("Server started on port 3000...")
 })
